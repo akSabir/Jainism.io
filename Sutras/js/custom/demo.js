2d07c195d1ebd3ff8f02eecb0e8de4d3a7cb8400
krutarthvora <krutarth.d.vora@gmail.com>
2016-08-23 23:16:06 +0530

    Update demo.js

--- Sutras/js/custom/demo.js
+++ Sutras/js/custom/demo.js
@@ -2,14 +2,14 @@
 // Here is how to define your module 
 // has dependent on mobile-angular-ui
 // 
-var app = angular.module('MobileAngularUiExamples', ['ngRoute', 'mobile-angular-ui',
+var app = angular.module('MobileAngularUiExamples', ['ngRoute','ngSanitize', 'mobile-angular-ui',
   // touch/drag feature: this is from 'mobile-angular-ui.gestures.js'
   // it is at a very beginning stage, so please be careful if you like to use
   // in production. This is intended to provide a flexible, integrated and and 
   // easy to use alternative to other 3rd party libs like hammer.js, with the
   // final pourpose to integrate gestures into default ui interactions like 
   // opening sidebars, turning switches on/off ..
-  'mobile-angular-ui.gestures','ngSanitize'
+  'mobile-angular-ui.gestures'
 ]);
 
 app.run(function ($transform) {
